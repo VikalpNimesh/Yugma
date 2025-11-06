f8abe6305762cc685062453792b0b668c38b66e6
Vaibhav Nimesh <vaibhavnimesh1@gmail.com>
2025-11-06 22:47:13 +0530

    add like and match

---
+++ src/screens/LikeList.tsx
@@ -0,0 +1,209 @@
+import { FlatList, Image, Pressable, StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import Ionicons from "react-native-vector-icons/Ionicons";
+import Feather from "react-native-vector-icons/Feather";
+import Octicons from "react-native-vector-icons/Octicons";
+
+const LikeList = ({ data }: any) => {
+
+    const renderItem = ({ item }: any) => (
+        <View style={styles.card}>
+            <View style={styles.row}>
+                <Image source={{ uri: item.image }} style={styles.avatar} />
+
+            </View>
+            <View style={{ flex: 1 }}>
+                <View style={styles.matchBadge}>
+                    <Text style={styles.name}>
+                        {item.name}, {item.age}
+                    </Text>
+                    <View style={styles.percent}>
+
+                        <Text style={styles.matchText}>{item.matchPercent} match</Text>
+                    </View>
+                </View>
+
+                <Text style={styles.subText}><Ionicons name="location-outline" size={12} /> {item.location}</Text>
+                <Text style={styles.subText}>{item.job}</Text>
+                <View style={styles.dateMsg}>
+
+                    <Text style={styles.matchDate}><Ionicons name="calendar-clear-outline" size={12} /> Matched {item.matchedDays}</Text>
+                    <Pressable style={styles.messageBtn}>
+                        <Feather name="message-circle" size={24} color={"white"} />
+                        <Text style={styles.messageText}>
+                            {item.verified ? "Message" : "Reply"}
+                        </Text>
+                    </Pressable>
+                </View>
+
+                <Pressable style={styles.verifyBtn}>
+                    <Octicons name="shield-check" size={18} color={"black"} />
+                    <Text style={styles.verifyBtnText}> Request Verification</Text>
+                </Pressable>
+                {item.verified && (
+                    <View style={styles.verifiedTag}>
+                        <Text style={styles.verifiedText}>Verified Profile</Text>
+                    </View>
+                )}
+
+            </View>
+
+        </View>
+    );
+    return (
+        <View style={{ flex: 1 }}>
+
+
+
+            <FlatList
+                ListHeaderComponent={() => (
+                    <View style={styles.headerCon}>
+
+                        <Text style={styles.header}>People who liked you
+                        </Text>
+                        <Text style={styles.subHeader}>
+                            These people have shown interest in your profile. Like them back to create a match!
+                        </Text>
+                    </View>
+                )}
+                data={data}
+                keyExtractor={(item) => item.id}
+                renderItem={renderItem}
+                showsVerticalScrollIndicator={false}
+                contentContainerStyle={{ paddingBottom: 40 }}
+            />
+        </View>
+    )
+}
+
+export default LikeList
+
+const styles = StyleSheet.create({
+    headerCon: {
+        backgroundColor: "#FFF6EE",
+        borderRadius: 14,
+        padding: 16,
+        marginBottom: 16
+    },
+    header: {
+        fontSize: 20,
+        fontWeight: "700",
+        color: "#1a1a1a",
+        marginBottom: 4
+    },
+    subHeader: {
+        fontSize: 16,
+        color: "#666",
+        lineHeight: 24
+    },
+    card: {
+        backgroundColor: "#fff",
+        borderRadius: 16,
+        padding: 14,
+        marginBottom: 16,
+        shadowColor: "#000",
+        shadowOpacity: 0.05,
+        shadowRadius: 2,
+        flexDirection: "row",
+        borderWidth: 1,
+        borderColor: "#dadada"
+    },
+    row: {
+        flexDirection: "row",
+    },
+    avatar: {
+        width: 60,
+        height: 60,
+        borderRadius: 30,
+        marginRight: 12,
+    },
+    name: {
+        fontSize: 16,
+        fontWeight: "700",
+        color: "#222",
+    },
+    subText: {
+        color: "#555",
+        fontSize: 13,
+        marginTop: 2,
+    },
+    matchDate: {
+        color: "#999",
+        fontSize: 12,
+        marginTop: 2,
+    },
+    verifiedTag: {
+        backgroundColor: "#d8f5dc",
+        paddingVertical: 4,
+        paddingHorizontal: 10,
+        borderRadius: 6,
+        marginTop: 6,
+        alignSelf: "flex-start",
+    },
+    verifiedText: {
+        color: "#177245",
+        fontWeight: "600",
+        fontSize: 12,
+    },
+    verifyBtn: {
+        borderWidth: 1,
+        borderColor: "#ffb700",
+        paddingVertical: 8,
+        paddingHorizontal: 8,
+        borderRadius: 8,
+        margin: 6,
+        marginTop: 10,
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "row",
+        gap: 6
+
+    },
+    verifyBtnText: {
+        color: "black",
+        fontWeight: "600",
+        fontSize: 14,
+        textAlign: "center",
+
+    },
+    matchBadge: {
+        // backgroundColor: "#f5f5f5",
+        paddingVertical: 4,
+        paddingHorizontal: 8,
+        borderRadius: 20,
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center"
+    },
+    matchText: {
+        fontSize: 12,
+        fontWeight: "600",
+        color: "#333",
+    },
+    messageBtn: {
+        backgroundColor: "black",
+        paddingVertical: 8,
+        borderRadius: 10,
+        alignItems: "center",
+        flexDirection: "row",
+        paddingHorizontal: 14,
+        gap: 6
+    },
+    messageText: {
+        color: "#fff",
+        fontWeight: "600",
+    },
+    dateMsg: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center"
+
+    },
+    percent: {
+        borderWidth: 1,
+        borderColor: "#dadada",
+        padding: 6,
+        borderRadius: 8,
+        paddingHorizontal: 10
+    }
+})
--- src/screens/MatchesScreen.tsx
+++ src/screens/MatchesScreen.tsx
@@ -7,9 +7,9 @@ import {
     Pressable,
     StyleSheet,
 } from "react-native";
-import Ionicons from "react-native-vector-icons/Ionicons";
-import Feather from "react-native-vector-icons/Feather";
-import Octicons from "react-native-vector-icons/Octicons";
+
+import MatchList from "./MatchList";
+import LikeList from "./LikeList";
 
 
 const matchesData = [
@@ -40,50 +40,7 @@ const matchesData = [
 const MatchesScreen = () => {
     const [selectedTab, setSelectedTab] = useState("Matches");
 
-    const renderItem = ({ item }: any) => (
-        <View style={styles.card}>
-            <View style={styles.row}>
-                <Image source={{ uri: item.image }} style={styles.avatar} />
 
-            </View>
-            <View style={{ flex: 1 }}>
-                <View style={styles.matchBadge}>
-                    <Text style={styles.name}>
-                        {item.name}, {item.age}
-                    </Text>
-                    <View style={styles.percent}>
-
-                        <Text style={styles.matchText}>{item.matchPercent} match</Text>
-                    </View>
-                </View>
-
-                <Text style={styles.subText}><Ionicons name="location-outline" size={12} /> {item.location}</Text>
-                <Text style={styles.subText}>{item.job}</Text>
-                <View style={styles.dateMsg}>
-
-                    <Text style={styles.matchDate}><Ionicons name="calendar-clear-outline" size={12} /> Matched {item.matchedDays}</Text>
-                    <Pressable style={styles.messageBtn}>
-                        <Feather name="message-circle" size={24} color={"white"} />
-                        <Text style={styles.messageText}>
-                            {item.verified ? "Message" : "Reply"}
-                        </Text>
-                    </Pressable>
-                </View>
-
-                <Pressable style={styles.verifyBtn}>
-                    <Octicons name="shield-check" size={18} color={"black"} />
-                    <Text style={styles.verifyBtnText}> Request Verification</Text>
-                </Pressable>
-                {item.verified && (
-                    <View style={styles.verifiedTag}>
-                        <Text style={styles.verifiedText}>Verified Profile</Text>
-                    </View>
-                )}
-
-            </View>
-
-        </View>
-    );
 
     return (
         <View style={styles.container}>
@@ -92,7 +49,7 @@ const MatchesScreen = () => {
                 Connect with compatible partners who share your values
             </Text>
 
-            {/* Tabs */}
+
             <View style={styles.tabContainer}>
                 {["Matches", "Likes"]?.map((tab) => (
                     <Pressable
@@ -115,13 +72,12 @@ const MatchesScreen = () => {
                 ))}
             </View>
 
-            <FlatList
-                data={matchesData}
-                keyExtractor={(item) => item.id}
-                renderItem={renderItem}
-                showsVerticalScrollIndicator={false}
-                contentContainerStyle={{ paddingBottom: 40 }}
-            />
+            {selectedTab == "Matches" ? <MatchList data={matchesData} /> :
+
+                <LikeList data={matchesData} />
+            }
+
+
         </View>
     );
 };
@@ -141,7 +97,7 @@ const styles = StyleSheet.create({
         color: "#1a1a1a",
     },
     subHeader: {
-        fontSize: 14,
+        fontSize: 16,
         color: "#666",
         marginVertical: 10,
     },
@@ -170,114 +126,5 @@ const styles = StyleSheet.create({
         color: "#000",
         fontWeight: "600",
     },
-    card: {
-        backgroundColor: "#fff",
-        borderRadius: 16,
-        padding: 14,
-        marginBottom: 16,
-        shadowColor: "#000",
-        shadowOpacity: 0.05,
-        shadowRadius: 2,
-        flexDirection: "row",
-        borderWidth: 1,
-        borderColor: "#dadada"
-    },
-    row: {
-        flexDirection: "row",
-    },
-    avatar: {
-        width: 60,
-        height: 60,
-        borderRadius: 30,
-        marginRight: 12,
-    },
-    name: {
-        fontSize: 16,
-        fontWeight: "700",
-        color: "#222",
-    },
-    subText: {
-        color: "#555",
-        fontSize: 13,
-        marginTop: 2,
-    },
-    matchDate: {
-        color: "#999",
-        fontSize: 12,
-        marginTop: 2,
-    },
-    verifiedTag: {
-        backgroundColor: "#d8f5dc",
-        paddingVertical: 4,
-        paddingHorizontal: 10,
-        borderRadius: 6,
-        marginTop: 6,
-        alignSelf: "flex-start",
-    },
-    verifiedText: {
-        color: "#177245",
-        fontWeight: "600",
-        fontSize: 12,
-    },
-    verifyBtn: {
-        borderWidth: 1,
-        borderColor: "#ffb700",
-        paddingVertical: 8,
-        paddingHorizontal: 8,
-        borderRadius: 8,
-        margin: 6,
-        marginTop: 10,
-        justifyContent: "center",
-        alignItems: "center",
-        flexDirection: "row",
-        gap: 6
 
-    },
-    verifyBtnText: {
-        color: "black",
-        fontWeight: "600",
-        fontSize: 14,
-        textAlign: "center",
-
-    },
-    matchBadge: {
-        // backgroundColor: "#f5f5f5",
-        paddingVertical: 4,
-        paddingHorizontal: 8,
-        borderRadius: 20,
-        flexDirection: "row",
-        justifyContent: "space-between",
-        alignItems: "center"
-    },
-    matchText: {
-        fontSize: 12,
-        fontWeight: "600",
-        color: "#333",
-    },
-    messageBtn: {
-        backgroundColor: "black",
-        paddingVertical: 8,
-        borderRadius: 10,
-        alignItems: "center",
-        flexDirection: "row",
-        paddingHorizontal: 14,
-        gap: 6
-    },
-    messageText: {
-        color: "#fff",
-        fontWeight: "600",
-    },
-    dateMsg: {
-        flexDirection: "row",
-        justifyContent: "space-between",
-        alignItems: "center"
-
-    },
-    percent: {
-        borderWidth: 1,
-        borderColor: "#dadada",
-        padding: 6,
-        borderRadius: 8,
-        paddingHorizontal: 10
-    }
 });
